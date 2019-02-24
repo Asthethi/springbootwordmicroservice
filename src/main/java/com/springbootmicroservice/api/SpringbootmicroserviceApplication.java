df3efd7303e6f8a85dfcc0305048dd71813ccc95
AMRITPAL SINGH THETHI <amritpalsinghthethi@gmail.com>
2019-02-24 16:02:59 +0530

    Comment change in Application Class

--- src/main/java/com/springbootmicroservice/api/SpringbootmicroserviceApplication.java
+++ src/main/java/com/springbootmicroservice/api/SpringbootmicroserviceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author AST
- * @version 1.0
+ * @author Asthethi
+ * @version SNAPSHOT 1.0
  */
 @SpringBootApplication
 public class SpringbootmicroserviceApplication {
