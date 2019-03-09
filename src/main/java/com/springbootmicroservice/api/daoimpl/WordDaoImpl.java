4d6a1ee39e3079fd5465711ccd233302b6741cf3
AMRITPAL SINGH THETHI <amritpalsinghthethi@gmail.com>
2019-03-09 13:11:49 +0530

    Third Commit Changes

--- src/main/java/com/springbootmicroservice/api/daoimpl/WordDaoImpl.java
+++ src/main/java/com/springbootmicroservice/api/daoimpl/WordDaoImpl.java
@@ -14,8 +14,7 @@ import com.springbootmicroservice.api.mappers.WordMapper;
 import com.springbootmicroservice.api.model.Word;
 
 /**
- * @author AST
- * Class Interacts with Database
+ * @author AST Class Interacts with Database
  */
 @Repository
 public class WordDaoImpl implements WordDao {
@@ -28,17 +27,17 @@ public class WordDaoImpl implements WordDao {
 
 		List<Word> allWords = jdbcTemplate.query(WordMicroserviceConstants.ALL_WORDS_QUERY, new WordMapper());
 		return allWords;
-	}//adfds
+	}// adfds
 
 	@Override
 	public Word saveWordObj(Word word) throws WordServiceBadRequestException {
 
-		if(word.getWord()==null || word.getMeaning()==null)
+		if (word.getWord() == null || word.getMeaning() == null)
 			throw new WordServiceBadRequestException("Word or Meaning cannot be null");
 		else {
 			try {
 				jdbcTemplate.update(WordMicroserviceConstants.INSERT_NEW_WORD,
-						new Object[] { word.getWord(), word.getMeaning()});
+						new Object[] { word.getWord(), word.getMeaning() });
 			} catch (DataAccessException dae) {
 				throw new WordServiceBadRequestException(dae.getMessage());
 			}
@@ -49,8 +48,8 @@ public class WordDaoImpl implements WordDao {
 
 	@Override
 	public List<Word> getWordById(int id) {
-		
-		List<Word> wordById = jdbcTemplate.query("SELECT * FROM WORDS WHERE ID = "+id, new WordMapper());
+
+		List<Word> wordById = jdbcTemplate.query("SELECT * FROM WORDS WHERE ID = " + id, new WordMapper());
 		return wordById;
 	}
 
